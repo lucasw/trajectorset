9a7787a4cfacf7c992f940f64febf0d16c795e21
binarymillenium <binarymillenium@3c8486b4-5c13-11de-8a4b-256bef446c6c>
2009-08-03 01:21:52 +0000

    I think this is a good basis, will try making a few dozen runs and seeing the dispersion.

--- trunk/plot2d_aggregate/gen_plots.m
+++ trunk/plot2d_aggregate/gen_plots.m
@@ -3,13 +3,20 @@ function veh = gen_plots()
 rand('twister',2);
 
 width = 100;
-tmax = 200;
+tmax = 450;
+gravity = -0.0020;
+acc = 0.0045;
+tx = 40;
+ty = 70;
+
+max_tdot = pi/20;
+
 %for i = [1:100]
     % generate a 2d wind landscape
     % > 0 means winds to east (or +x)
     % < 0 means winds to west (or -x)
-    windx = 0.05*(perlin(width,8) -0.5);
-    windy = 0.009*(perlin(width,8) -0.5);
+    windx = 0.004*(perlin(width,8) -0.5);
+    windy = 0.002*(perlin(width,8) -0.5);
     
     veh.x = zeros(1,tmax);
     veh.y = zeros(1,tmax);
@@ -19,6 +26,8 @@ tmax = 200;
     veh.ay = zeros(1,tmax);
     veh.theta = zeros(1,tmax);
     veh.thetadot = zeros(1,tmax);
+    veh.theta_target = zeros(1,tmax);
+    veh.theta_target2 = zeros(1,tmax);
     
     veh.x(1) = width/2;
     veh.y(1) = 0;
@@ -26,10 +35,39 @@ tmax = 200;
     veh.vx(1) = 0;
     veh.vy(1) = 0;
     
+
+  
     for t = [2:tmax]
         
-        veh.ax(t) = 0;
-        veh.ay(t) = 0.0025;
+        dx = tx-veh.x(t-1);
+        dy = ty-veh.y(t-1);
+        dist = sqrt(dx*dx+dy*dy);
+        
+        veh.theta_target(t) = atan2( dx,dy );
+        veh.theta_target(1:t) = unwrap(veh.theta_target(1:t));
+        
+        veh.theta_target2(t) = atan2(veh.vx(t-1),veh.vy(t-1));
+        veh.theta_target2(1:t) = unwrap(veh.theta_target2(1:t));
+        
+        veh.thetadot(t) = 0.1*(veh.theta_target(t) - veh.theta(t-1) - 0.3*veh.theta_target2(t));
+           
+        if (veh.thetadot(t) > max_tdot)
+            veh.thetadot(t) = max_tdot;
+        elseif (veh.thetadot(t) < -max_tdot)
+            veh.thetadot(t) = -max_tdot;
+        end
+        
+        veh.theta(t) = veh.theta(t-1) + veh.thetadot(t);
+        
+%         if (veh.theta(t) > pi) 
+%             veh.theta(t) = veh.theta(t)-2*pi;
+%         elseif (veh.theta(t) < -pi)
+%             veh.theta(t) = veh.theta(t)+2*pi;
+%         end
+            
+        real_acc = acc*(0.6 + 0.4*dist/width);
+        veh.ax(t) = sin(veh.theta(t))*real_acc;
+        veh.ay(t) = cos(veh.theta(t))*real_acc + gravity;
         
         veh.vy(t) = veh.vy(t-1) + veh.ay(t);
         veh.vy(t) = veh.vy(t) + interp2(windy,veh.x(t-1),veh.y(t-1),'linear',0);
@@ -49,12 +87,12 @@ tmax = 200;
            veh.x(t) = veh.x(t) + width;
         end
         
-        if veh.y(t) > width
-            veh.y(t) = veh.y(t) -width;
-        end
+%         if veh.y(t) > width
+%             veh.y(t) = veh.y(t) -width;
+%         end
         
         if (veh.y(t) < 0)
-           veh.y(t) = veh.y(t) + width;
+           veh.y(t) = 0; % veh.y(t) + width;
         end
         
     end
@@ -74,10 +112,30 @@ tmax = 200;
     plot(veh.x, veh.y);
     
     figure(2),
-    subplot(2,1,1);
+        subplot(4,1,1);
+    plot(veh.time, veh.x);
+    title('x');
+    subplot(4,1,2);
+    plot(veh.time, veh.y);
+    title('y');
+    subplot(4,1,3);
     plot(veh.time, veh.vx);
     title('vx');
-    subplot(2,1,2);
+    subplot(4,1,4);
+    plot(veh.time, veh.vy);
+    title('vy');
+    
+        figure(3),
+        subplot(4,1,1);
+    plot(veh.time, veh.theta,veh.time, veh.theta_target);
+    title('theta rad');
+    subplot(4,1,2);
+    plot(veh.time, veh.thetadot);
+    title('thetadot rad/s');
+    subplot(4,1,3);
+    plot(veh.time, veh.theta_target2);
+    title('theta target');
+    subplot(4,1,4);
     plot(veh.time, veh.vy);
     title('vy');
 %     subplot(2,1,2);
